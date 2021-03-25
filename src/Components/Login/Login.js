a06e6519643881f3d6799eb4b562ac8e2c8ea3fa
wmaneesh <wmaneesh@outlook.com>
2021-03-25 18:54:57 -0400

    more testing

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -45,7 +45,7 @@ const Login = (props) => {
         if (result.user_type === undefined) {
           setStatusMessage(result);
         } else {
-          props.setAccountType(`/${result.user_type}`);
+          props.setAccountType(`/medmemo/${result.user_type}`);
           props.setAuthenticate(true);
           props.setUserToken(token);
         }
