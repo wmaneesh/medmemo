ce7f8aa6931f949ef54cccf37000228f5a1c0f3e
wmaneesh <wmaneesh@outlook.com>
2021-03-25 16:50:50 -0400

    removed port declaration

--- src/App.js
+++ src/App.js
@@ -37,7 +37,7 @@ function App() {
 
   console.log("states are:", accountType, authenticate, userToken);
   useEffect(() => {
-    fetch("https://159.203.29.156:443/isAuthenticated")
+    fetch("https://159.203.29.156/isAuthenticated")
       .then((res) => {
         if (res.ok) {
           return res.json();
