513483325989a513756d176dbadd5a0c024c7f61
wmaneesh <wmaneesh@outlook.com>
2021-03-25 13:52:32 -0400

    test

--- src/App.js
+++ src/App.js
@@ -35,7 +35,7 @@ function App() {
   const [userToken, setUserToken] = useState("");
   //"proxy": "https://159.203.29.156:443",
   useEffect(() => {
-    fetch("https://159.203.29.156:443/isAuthenticated")
+    fetch("http://159.203.29.156:80/isAuthenticated")
       .then((res) => {
         if (res.ok) {
           return res.json();
