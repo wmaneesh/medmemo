024cecdbee72ed1374f53402d2e0ca12abedcddb
wmaneesh <wmaneesh@outlook.com>
2021-03-25 18:52:56 -0400

    more testing

--- src/App.js
+++ src/App.js
@@ -85,7 +85,7 @@ function App() {
         {accountType !== "" ? (
           <Switch>
             <ProtectedRoute
-              path="/nurse"
+              path="medmemo/nurse"
               component={NursePage}
               search={search}
               setSearch={setSearch}
