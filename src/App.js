a3a3fe958f6d167060effdfc07dc08497a10ac2d
wmaneesh <wmaneesh@outlook.com>
2021-03-25 15:24:25 -0400

    removing ssl

--- src/App.js
+++ src/App.js
@@ -37,7 +37,7 @@ function App() {
 
   console.log("states are:", accountType, authenticate, userToken);
   useEffect(() => {
-    fetch("https://159.203.29.156:443/isAuthenticated")
+    fetch("http://159.203.29.156:80/isAuthenticated")
       .then((res) => {
         if (res.ok) {
           return res.json();
