5bd38dcb2c392512d4f5f62a61adef84d7455a62
wmaneesh <wmaneesh@outlook.com>
2021-03-25 20:26:09 -0400

    more testing

--- src/App.js
+++ src/App.js
@@ -37,9 +37,7 @@ function App() {
 
   console.log("states are:", accountType, authenticate, userToken);
   useEffect(() => {
-    fetch("https://159.203.29.156/isAuthenticated", {
-      credentials: "include",
-    })
+    fetch("https://159.203.29.156/isAuthenticated")
       .then((res) => {
         if (res.ok) {
           return res.json();
