af4037f255dc04a8962b76ef6574f63ae24c7341
Maneesh Withanagamage <wmaneesh@outlook.com>
2021-05-06 10:49:59 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -118,12 +118,12 @@ function App() {
         ) : (
           <div className={classes.root}>
             <h1>Capstone project has ended and MedMemo will be offline</h1>
-            <MuiThemeProvider theme={themes}>
+            {/* <MuiThemeProvider theme={themes}>
               <CircularProgress
                 style={{ position: "fixed", top: "30%", left: "50%" }}
                 size={200}
               />
-            </MuiThemeProvider>
+            </MuiThemeProvider> */}
           </div>
         )}
       </Router>
